682f1dbc604c6a0ce377df0984c843590a91312c
Jarred Capellman <jcapellman@hotmail.com>
2024-07-22 06:38:24 -0400

    Added: Feature Importance plotting

--- rvepp-ml-trainer/trainer.py
+++ rvepp-ml-trainer/trainer.py
@@ -1,9 +1,8 @@
 import lightgbm as lgb
 import pandas as pd
-#import matplotlib as mpt
-
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score as ras
+import matplotlib.pyplot as plt
 
 # Defaults
 model_file_name = 'elf.mdl'
@@ -28,14 +27,15 @@ if verbose_logging:
 features = df.drop('is_malicious', axis=1)
 target = df['is_malicious']
 
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+X_train, X_val, Y_train, Y_val = train_test_split(features, target, test_size=0.2, random_state=42)
 
-train_data = lgb.Dataset(X_train, label=y_train)
+train_data = lgb.Dataset(X_train, label=Y_train)
+test_data = lgb.Dataset(X_val, label=Y_val, reference=train_data)
 
 params = {
     'boosting_type': 'gbdt',
     'objective': 'binary',
-    'metric': 'binary_logloss',
+    'metric': 'auc',
     'num_leaves': 5,
     'learning_rate': 0.05,
     'feature_fraction': 0.9
@@ -45,11 +45,12 @@ bst = lgb.train(params, train_data)
 
 bst.save_model(model_file_name)
 
-#lgb.plot_importance(bst, height=.5)
+lgb.plot_importance(bst, height=.5)
+plt.show()
 
-y_pred = bst.predict(X_test, num_iteration=bst.best_iteration)
+y_pred = bst.predict(X_val, num_iteration=bst.best_iteration)
 
 y_pred_binary = [1 if pred > 0.5 else 0 for pred in y_pred]
 
-accuracy = accuracy_score(y_test, y_pred_binary)
-print(f'Accuracy: {accuracy}')
+accuracy = accuracy_score(Y_val, y_pred_binary)
+print(f'Test Set Accuracy: {accuracy}')
