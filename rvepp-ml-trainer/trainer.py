9848ce8fcdfe629f4f6c75550c7263c36ed6ef4d
Jarred Capellman <jcapellman@hotmail.com>
2024-07-23 17:53:51 -0400

    Change: Moved the config and argument parsing to it's own file

--- rvepp-ml-trainer/trainer.py
+++ rvepp-ml-trainer/trainer.py
@@ -1,33 +1,11 @@
 import lightgbm as lgb
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
-import argparse
 
-DEFAULT_MODEL_FILE_NAME = 'elf.mdl'
-
-
-class Config:
-    verbose_logging = False
-    model_file_name = DEFAULT_MODEL_FILE_NAME
-
-    def __init__(self, verbose_logging, model_file_name):
-        self.verbose_logging = verbose_logging
-        self.model_file_name = model_file_name
-
-
-def parse_arguments() -> Config:
-    # Instantiate the parser
-    parser = argparse.ArgumentParser(description='Trainer Application for RVEPP')
-
-    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
-    parser.add_argument('-mo', '--modeloutput', type=str, default=DEFAULT_MODEL_FILE_NAME, help='Output model file')
-
-    args = parser.parse_args()
-
-    return Config(args.verbose, args.modeloutput)
-
+from trainer_config import parse_arguments
 
 config = parse_arguments()
 
