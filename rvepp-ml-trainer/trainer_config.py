d358ddb26aa03250666a2aa3b13a318ac993c794
Jarred Capellman <jcapellman@hotmail.com>
2024-07-24 07:27:36 -0400

    Added: Type Hints and fixed the PEP 8 line length

--- rvepp-ml-trainer/trainer_config.py
+++ rvepp-ml-trainer/trainer_config.py
@@ -6,9 +6,9 @@ DEFAULT_TRAINING_SET_FILE_NAME = '../DataSets/Synthetic/Test001.csv'
 
 
 class Config:
-    verbose_logging = False
-    model_file_name = DEFAULT_MODEL_FILE_NAME
-    training_set_file_name = DEFAULT_TRAINING_SET_FILE_NAME
+    verbose_logging: bool = False
+    model_file_name: str = DEFAULT_MODEL_FILE_NAME
+    training_set_file_name: str = DEFAULT_TRAINING_SET_FILE_NAME
 
     def __init__(self, verbose_logging, model_file_name, training_set_file_name):
         self.verbose_logging = verbose_logging
@@ -21,8 +21,10 @@ def parse_arguments() -> Config:
     parser = argparse.ArgumentParser(description='Trainer Application for RVEPP')
 
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
-    parser.add_argument('-mo', '--modeloutput', type=str, default=DEFAULT_MODEL_FILE_NAME, help='Output model file')
-    parser.add_argument('-ts', '--trainingset', type=str, default=DEFAULT_TRAINING_SET_FILE_NAME, help='Training set input file')
+    parser.add_argument('-mo', '--modeloutput', type=str, default=DEFAULT_MODEL_FILE_NAME,
+                        help='Output model file')
+    parser.add_argument('-ts', '--trainingset', type=str, default=DEFAULT_TRAINING_SET_FILE_NAME,
+                        help='Training set input file')
 
     args = parser.parse_args()
 
