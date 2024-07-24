8234e610f33d7a8fb944801e6518a4ef6cb6dd6d
Jarred Capellman <jcapellman@hotmail.com>
2024-07-23 23:16:03 -0400

    Change: Updated the LightGBM code to use the DataSet object instead of separate values

--- rvepp-ml-trainer/trainer_lgbm.py
+++ rvepp-ml-trainer/trainer_lgbm.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 
 
-def train_model(config, x_train, x_val, y_train, y_val):
+def train_model(config, data_set):
     print('Training with LightGBM (Version ' + lgb.__version__ + ')')
 
-    train_data = lgb.Dataset(x_train, label=y_train)
-    test_data = lgb.Dataset(x_val, label=y_val, reference=train_data)
+    train_data = lgb.Dataset(data_set.x_train, label=data_set.y_train)
+    test_data = lgb.Dataset(data_set.x_val, label=data_set.y_val, reference=train_data)
 
     params = {
         'boosting_type': 'gbdt',
@@ -28,9 +28,9 @@ def train_model(config, x_train, x_val, y_train, y_val):
     lgb.plot_importance(bst, height=.5)
     plt.show()
 
-    y_pred = bst.predict(x_val, num_iteration=bst.best_iteration)
+    y_pred = bst.predict(data_set.x_val, num_iteration=bst.best_iteration)
 
     y_pred_binary = [1 if pred > 0.5 else 0 for pred in y_pred]
 
-    accuracy = accuracy_score(y_val, y_pred_binary)
+    accuracy = accuracy_score(data_set.y_val, y_pred_binary)
     print(f'Test Set Accuracy: {accuracy}')
