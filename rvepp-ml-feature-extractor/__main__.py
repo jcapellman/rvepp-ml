c4ee103fdae7881a34a022417740a0bfed5ba824
Jarred Capellman <jcapellman@hotmail.com>
2024-07-26 12:46:45 -0400

    Added: Verbose output at startup of the Feature Extractor

--- rvepp-ml-feature-extractor/__main__.py
+++ rvepp-ml-feature-extractor/__main__.py
@@ -3,6 +3,11 @@ from fe_synthetic_generator import SyntheticDataGenerator
 
 config = parse_arguments()
 
+print('RVEPP-ML Feature Extractor (2024.7.0)')
+
+if config.verbose_logging:
+    print('Using the following config: ' + config.to_json())
+
 match config.extraction_type:
     case ExtractionTypes.SYNTHETIC:
         SyntheticDataGenerator().run_extraction(config)
