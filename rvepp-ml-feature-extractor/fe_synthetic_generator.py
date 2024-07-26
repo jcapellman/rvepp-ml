869aa9fad6fdf897f356eb09ff577020de06529c
Jarred Capellman <jcapellman@hotmail.com>
2024-07-26 12:49:24 -0400

    Change: Replaced the fe_synthetic_generator.py hard coded values with a dynamic configuration

--- rvepp-ml-feature-extractor/fe_synthetic_generator.py
+++ rvepp-ml-feature-extractor/fe_synthetic_generator.py
@@ -3,17 +3,18 @@ import random
 
 from fe_config import Config
 from fe_extractor import Extractor, Feature
+from fe_synthetic_generator_config import SyntheticGeneratorConfig
 
 
 class SyntheticDataGenerator(Extractor):
     def run_extraction(self, config: Config) -> bool:
+        data_config = SyntheticGeneratorConfig.load_from_file(config.extraction_config_file_name)
+
         data_file = io.open(config.output_file, mode='w')
 
-        random.seed(1985)
+        random.seed(data_config.seed_value)
 
-        dataset_size = 1000
-
-        for i in range(dataset_size):
+        for i in range(data_config.dataset_size):
             is_malicious = random.randint(0, 1)
 
             if is_malicious == 0:
@@ -34,5 +35,7 @@ class SyntheticDataGenerator(Extractor):
             super().write_row(data_file, Feature(is_malicious, file_size, is_packed))
 
         data_file.close()
+
+        print('Generated a Synthetic Data Set of (' + str(data_config.dataset_size) + ' rows) to ' + config.output_file)
 
         return True
