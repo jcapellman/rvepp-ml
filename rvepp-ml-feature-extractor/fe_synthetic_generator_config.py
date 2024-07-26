9b2bb96d6c427feed11a7f699efb0a3318ae6041
Jarred Capellman <jcapellman@hotmail.com>
2024-07-26 12:50:16 -0400

    Added: fe_synthetic_generator_config class

---
+++ rvepp-ml-feature-extractor/fe_synthetic_generator_config.py
@@ -0,0 +1,17 @@
+from common_json_file_loader import load_from_file
+
+DEFAULT_SEED_VALUE: int = 1985
+DEFAULT_DATASET_SIZE: int = 2003
+
+
+class SyntheticGeneratorConfig:
+    seed_value: int
+    dataset_size: int
+
+    def __init__(self, seed_value: int = DEFAULT_SEED_VALUE, dataset_size: int = DEFAULT_DATASET_SIZE):
+        self.seed_value = seed_value
+        self.dataset_size = dataset_size
+
+    @staticmethod
+    def load_from_file(file_name: str):
+        return load_from_file(file_name, SyntheticGeneratorConfig)
