e8b96f4282c18b0edc39567e115afb25c76b317a
Jarred Capellman <jcapellman@hotmail.com>
2024-07-27 18:57:13 -0400

    Fix: Variable Type discrepancies

--- rvepp_ml_feature_extractor/fe_synthetic_generator.py
+++ rvepp_ml_feature_extractor/fe_synthetic_generator.py
@@ -15,22 +15,22 @@ class SyntheticDataGenerator(Extractor):
         random.seed(data_config.seed_value)
 
         for i in range(data_config.dataset_size):
-            is_malicious = random.randint(0, 1)
+            is_malicious = bool(random.randint(0, 1))
 
             if is_malicious == 0:
                 file_size = random.randint(200, 2000)
 
                 if file_size > 1500:
-                    is_packed = random.randint(0, 1)
+                    is_packed = bool(random.randint(0, 1))
                 else:
-                    is_packed = 1
+                    is_packed = True
             else:
                 file_size = random.randint(1000, 10000)
 
                 if file_size > 2500:
-                    is_packed = random.randint(0, 1)
+                    is_packed = bool(random.randint(0, 1))
                 else:
-                    is_packed = 0
+                    is_packed = False
 
             super().write_row(data_file, Feature(is_malicious, file_size, is_packed))
 
