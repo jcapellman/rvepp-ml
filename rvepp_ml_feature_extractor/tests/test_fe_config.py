74d65295ac16728b589108f6f5765ed33dd1cb11
Jarred Capellman <jcapellman@hotmail.com>
2024-07-28 07:02:47 -0500

    Fix: E302 Linter error

--- rvepp_ml_feature_extractor/tests/test_fe_config.py
+++ rvepp_ml_feature_extractor/tests/test_fe_config.py
@@ -2,6 +2,7 @@ import unittest
 
 from rvepp_ml_feature_extractor import fe_config
 
+
 class ConfigTests(unittest.TestCase):
     def test_load_config_null_arg(self):
         with self.assertRaises(ValueError):
