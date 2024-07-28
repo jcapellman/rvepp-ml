730bf1a0e9a47f0808e086bae3c82ebac73ea1a1
Jarred Capellman <jcapellman@hotmail.com>
2024-07-28 09:36:22 -0500

    Added: Additional output in the Evaluator

--- rvepp_ml_evaluator/__main__.py
+++ rvepp_ml_evaluator/__main__.py
@@ -8,6 +8,10 @@ print('RVEPP-ML Evaluator (2024.7.0)')
 if config.verbose_logging:
     print('Using the following config: ' + config.to_json())
 
+print('Running the LightGBM evaluator...')
+
 model_metrics = run_evaluation(config)
 
 model_metrics.save_to_disk(config)
+
+print('Model Metrics were saved to ' + config.metrics_output_file_name)
